970bc1059483bd1179e0227c3d01614fd8345cd5
sobhandip1 <sobhandip.samanta@zensar.com>
2020-10-10 11:50:50 +0530

    Adding Angular assignment using Web Api

---
+++ ProductWebApi/ProductWebApi/Migrations/20201009144520_adding table Product.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProductWebApi.Migrations
+{
+    public partial class addingtableProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(nullable: true),
+                    ProductCode = table.Column<string>(nullable: true),
+                    ReleaseDate = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Price = table.Column<double>(nullable: false),
+                    StarRating = table.Column<double>(nullable: false),
+                    ImageUrl = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ ProductWebApi/ProductWebApi/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductWebApi.Models
+{
+    public class Product
+    {
+        
+        [Key]
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductCode { get; set; }
+        public string ReleaseDate { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public double StarRating { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
